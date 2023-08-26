28efad683db74bcb9bb1a87acf1b201570e6b2c3
Miguel Lares <siendomigueldev@gmail.com>
2023-08-25 22:34:49 -0500

    agregando CORS

--- app.js
+++ app.js
@@ -24,6 +24,14 @@ databaseConnection.on('error', (err) => {
 app.use(json())
 app.use(cors())
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000')
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE')
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type')
+  res.setHeader('Access-Control-Allow-Credentials', 'true')
+  next()
+})
+
 app.get('/', (req, res) => {
   res.send({ message: 'Mi API' })
 })
